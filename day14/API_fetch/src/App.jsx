466839fe3e86c5d6f2387ae914aecaa4388c10ef
Prince Rathi <princerathi674@gmail.com>
2026-06-25 10:22:23 +0530

    update and files

---
+++ day14/API_fetch/src/App.jsx
@@ -0,0 +1,14 @@
+import UserFetch from "./components/UserFetch"
+import './App.css'
+
+// import React from 'react'
+
+const App = () => {
+  return (
+    <div id="main">
+      <UserFetch />
+    </div>
+  )
+}
+
+export default App
